42deeea8f510f26442400372216c5572886c7e16
wirttipereira <wirttipereira@wirttipereiras-MacBook-Pro.local>
2020-07-07 17:27:51 -0300

    mac

--- density-region/varios.sh
+++ density-region/varios.sh
@@ -1,4 +1,15 @@
+> data.temp
+> data.temp2
+
 for i in {1..80}
 do
 python density-region.py  ../density-waterfal/ns$(($i)).xvg 19 30
 done
+
+
+j=100
+while read LINE
+do
+  echo "$j $LINE" >> data.temp2
+  j=$(($j+100))
+done < data.temp 
