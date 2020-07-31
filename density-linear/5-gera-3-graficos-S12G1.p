c980d24c923166a9b2d66da78f4f5b90fe943f2a
wirttipereira <wirttipereira@hotmail.com>
2020-07-31 00:17:17 -0300

    script automatico

--- density-linear/5-gera-3-graficos-S12G1.p
+++ density-linear/5-gera-3-graficos-S12G1.p
@@ -1,25 +1,29 @@
-set term x11 size 1200,600 title 'AS0DB80'
+set term png size 1200,600
+set output 'dlAS0DB80.png'
+#set term x11 size 1200,600 title 'AS0DB80'
 set multiplot layout 1,3
 
-set yrange [0:14]
+set yrange [0:5]
 set xlabel 'Z / nm'
 set ylabel 'Number density / nm-3'
 
-p '01ns250.xvg'   u 1:6 w l,\
-  '02ns750.xvg'   u 1:6 w l,\
-  '03ns1750.xvg'  u 1:6 w l,\
-  '04ns2750.xvg'  u 1:6 w l,\
-  '05ns4750.xvg'  u 1:6 w l,\
+p '01ns250.xvg'   u 1:6 w l lw 3 smooth bezier,\
+  '02ns750.xvg'   u 1:6 w l lw 3 smooth bezier,\
+  '03ns1750.xvg'  u 1:6 w l lw 3 smooth bezier,\
+  '04ns2750.xvg'  u 1:6 w l lw 3 smooth bezier,\
+  '05ns4750.xvg'  u 1:6 w l lw 3 smooth bezier,\
+  '06ns7250.xvg'  u 1:6 w l lw 3 smooth bezier
 
-p '06ns7250.xvg'  u 1:6 w l,\
-  '07ns9750.xvg'  u 1:6 w l,\
-  '08ns14750.xvg' u 1:6 w l,\
-  '09ns19250.xvg' u 1:6 w l,\
-  '10ns24750.xvg' u 1:6 w l,\
+p '07ns9750.xvg'  u 1:6 w l lw 3 smooth bezier,\
+  '08ns14750.xvg' u 1:6 w l lw 3 smooth bezier,\
+  '09ns19250.xvg' u 1:6 w l lw 3 smooth bezier,\
+  '10ns24750.xvg' u 1:6 w l lw 3 smooth bezier,\
+  '11ns29750.xvg' u 1:6 w l lw 3 smooth bezier,\
+  '12ns39250.xvg' u 1:6 w l lw 3 smooth bezier
 
-p '11ns29750.xvg' u 1:6 w l,\
-  '12ns39250.xvg' u 1:6 w l,\
-  '13ns49250.xvg' u 1:6 w l,\
-  '14ns59250.xvg' u 1:6 w l,\
-  '15ns69250.xvg' u 1:6 w l,\
-  '16ns80250.xvg' u 1:6 w l
+p '13ns49250.xvg' u 1:6 w l lw 3 smooth bezier,\
+  '14ns59250.xvg' u 1:6 w l lw 3 smooth bezier,\
+  '15ns69250.xvg' u 1:6 w l lw 3 smooth bezier,\
+  '16ns80250.xvg' u 1:6 w l lw 3 smooth bezier,\
+  '17ns90250.xvg' u 1:6 w l lw 3 smooth bezier,\
+  '18ns99000.xvg' u 1:6 w l lw 3 smooth bezier
--- status.sh
+++ status.sh
@@ -1,32 +1,40 @@
-echo AS2DB80
-cd /home/rafagp/Github/S12g1/padua/x50/500-co2/2V-from-lammps/80ns-350K/inteiro-eniac
-tail -n 15 md_80_100.part0006.log | grep -A 2 Time
+#echo AS2DB80
+#cd /home/rafagp/Github/S12g1/padua/x50/500-co2/2V-from-lammps/80ns-350K/inteiro-eniac
+#tail -n 15 md_80_100.part0006.log | grep -A 2 Time
 
-echo AS0DB80
-cd /home/rafagp/Github/S12g1/padua/x50/500-co2/0V-from-lammps/80ns-350K 
-tail -n 15 md_80_100.part0007.log | grep -A 2 Time
+#echo AS0DB80
+#cd /home/rafagp/Github/S12g1/padua/x50/500-co2/0V-from-lammps/80ns-350K 
+#tail -n 15 md_80_100.part0007.log | grep -A 2 Time
 
 echo AS2QB80
 cd /home/rafagp/Github/S12g1/padua/x50/250-co2/2V-from-lammps/80ns-350K
 tail -n 15 md.log | grep -A 2 Time #head -n 3
 
-echo AN2DB80
-cd /home/rafagp/Github/N1114/2V/D-500-co2/B-350K
-tail -n 15 md.part0005.log | grep -A 2 Time #head -n 3
+#echo AN2DB80
+#cd /home/rafagp/Github/N1114/2V/D-500-co2/B-350K
+#tail -n 15 md.part0005.log | grep -A 2 Time #head -n 3
 
-echo AN0DB80
-cd /home/rafagp/Github/N1114/0V/D-500-co2/B-350K
-tail -n 15 md.log | grep -A 2 Time #head -n 3
+#echo AN0DB80
+#cd /home/rafagp/Github/N1114/0V/D-500-co2/B-350K
+#tail -n 15 md.log | grep -A 2 Time #head -n 3
 
 echo AS0QB80
 cd /home/rafagp/Github/S12g1/padua/x50/250-co2/0V/B-350K
-tail -n 15 md.part0003.log | grep -A 2 Time #head -n 3
+tail -n 15 md.part0005.log | grep -A 2 Time #head -n 3
 
-echo AN2QB80
-cd /home/rafagp/Github/N1114/2V/Q-250-co2/B-350K
-tail -n 15 md.part0003.log | grep -A 2 Time #head -n 3
+#echo AN2QB80
+#cd /home/rafagp/Github/N1114/2V/Q-250-co2/B-350K
+#tail -n 15 md.part0004.log | grep -A 2 Time #head -n 3
 
-echo AN0QB80
-cd /home/rafagp/Github/N1114/0V/Q-250-co2/B-350K
-tail -n 15 md.part0003.log | grep -A 2 Time #head -n 3
+#echo AN0QB80
+#cd /home/rafagp/Github/N1114/0V/Q-250-co2/B-350K
+#tail -n 15 md.part0004.log | grep -A 2 Time #head -n 3
+
+echo AP2DB80
+cd /home/rafagp/Github/P2O1/2.5V/D-500-co2/B-350K
+tail -n 15 md.log | grep -A 2 Time
+
+echo AP2QB80
+cd /home/rafagp/Github/P2O1/2.5V/Q-250-co2/B-350K
+tail -n 15 md.log | grep -A 2 Time
 
