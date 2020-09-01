40ae73f4c813a8216759b7f4d233c6d08e91eac6
wirttipereira <wirttipereira@hotmail.com>
2020-09-01 00:06:20 -0300

    script automatico

--- min-max-z.py
+++ min-max-z.py
@@ -2,13 +2,26 @@
 
 import sys
 
+def mensagem():
+	print '##############################################################'
+	print 'Use: python min-max.py file.gro box_width option'
+	print '-> string: use "a" to all or the residue desired (ex. electr) '
+	print '##############################################################'
 
-arq_in=sys.argv[1]
-box_width=float(sys.argv[2])
-option=sys.argv[3]
+
+try:
+	arq_in=sys.argv[1]
+	box_width=float(sys.argv[2])
+	option=sys.argv[3]
+except:
+	mensagem()
+	sys.exit("arguments missing\n\n")
+
 
 minimo=999
 maximo=-999
+
+
 
 with open(arq_in) as f:
 	data_f = f.readlines()
@@ -16,11 +29,7 @@ with open(arq_in) as f:
 		#z = float(line_f[0:8])
 		z = float(line_f[37:44])
 		if(option=='h'):
-			print '################################################'
-			print 'Use: python min-max.py file.gro box_width option'
-			print '-> string: use "a" to all or the residue desired (ex. electr) '
-			print '################################################'
-			break	
+				sys.exit("bye bye")	
 		if(option=='a'):
 			if(z>maximo):
 				maximo = z
@@ -42,4 +51,4 @@ larg_eletr = maximo-minimo
 d = box_width/2 - larg_eletr/2 - minimo
 
 print 'min:',minimo,'  max:',maximo,'  eletrodo:',larg_eletr,'   deslocar em z: ',d
-
+print 'gmx editconf -f ',arq_in, '  -translate  0.  0.  ',d
