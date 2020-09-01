26aa3352e66d422bc72cf17f4a517a1381a1752c
wirttipereira <wirttipereira@wirttipereiras-MacBook-Pro.local>
2020-09-01 14:33:25 -0300

    script automatico

--- umbrella-sampling/pmf.py
+++ umbrella-sampling/pmf.py
@@ -1,21 +1,73 @@
-#!/usr/local/bin/python3
-#!/usr/bin/python3
-##!/scratch/app/python/3.8.2-gcc-7.4-gnu/bin/python3
+#!/usr/bin/env python
+# coding: utf-8
 
-##RGP - 1.9.2020
-##module load python/3.8.2
+# In[1]:
+
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+# In[2]:
+
+
 perfil200=pd.read_csv("profile-200.xvg", skiprows=(16), delimiter="\t",names=["x","y"])
+
+
+# Verifica as primeiras linhas, se importou direito
+
+# In[3]:
+
+
 print (perfil200.head())
+
+
+# Inverte o sinal de x
+
+# In[4]:
+
 
 perfil200["x"]=perfil200["x"]*-1
 
-print perfil200.min(level="y"), perfil200.max(level="y")
 
-perfil200.plot.line(x="x",y="y")
+# Imprime o máximo e o mínimo da curva
+
+# In[5]:
+
+
+#perfil200.min()
+y_min=perfil200["y"].min()
+y_max=perfil200["y"].max()
+delta=y_max - y_min
+print('min:',y_min, 'max:',y_max,'delta:',delta)
+
+
+# gera o gráfico
+
+# In[6]:
+
+
+p = perfil200.plot.line(x="x",y="y", grid=True, legend=False,figsize=(15,10))
+p.set_title("PMF exp7", size=40)
+p.set_xlabel("Distance / nm", size=20)
+p.set_ylabel("Energy / KJ.mol-1", size=20)
+p.text(2.2,-35,'min:'+str(y_min), size=20)
+p.text(7.5,-10,'max:'+str(y_max),size=20)
+p.text(7,-30,'delta:'+str(delta),size=20)
+p.tick_params(labelsize=20)
 plt.show()
+
+
+# Salva o plot em arquivo
+
+# In[7]:
+
+
+p.get_figure().savefig("pmfEXP7.20200901.png")
+
+
+# In[ ]:
+
+
 
 
