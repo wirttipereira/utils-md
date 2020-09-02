f9d3e4d76553c2885dd05ba65d8462df5e5c8944
Rafael Guimaraes Pereira <wirttipereira@hotmail.com>
2020-09-02 07:06:53 -0300

    script automatico

--- umbrella-sampling/1.3-gera-jobs-santos-dumont.sh
+++ umbrella-sampling/1.3-gera-jobs-santos-dumont.sh
@@ -12,7 +12,7 @@ while IFS= read line
 do
 	echo "$line"
 	#cp ./gro/conf"$line".gro .
-#	$gmx grompp -p "$top" -f "$mdp" -c ./gro/conf"$line".gro -o conf"$line".tpr -maxwarn 2
+	$gmx grompp -p "$top" -f "$mdp" -c ./gro/conf"$line".gro -o conf"$line".tpr -maxwarn 2
 	
 	cp run.srm run$line.srm
 	sed -i '1,$s/XX/'$line'/g' run$line.srm
