76781cf7424e039e95e5f000bb614adea1c696a1
Rafael Guimaraes Pereira <wirttipereira@hotmail.com>
2020-09-03 23:23:58 -0300

    script automatico

--- umbrella-sampling/1.2-gera-gro-escolhidos.sh
+++ umbrella-sampling/1.2-gera-gro-escolhidos.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-module load gromacs/5.1.5_openmpi_gnu
 
-gmx=gmx_mpi
-file="escolhidos.dat"
+##--- SANTOS DUMONT
+#module load gromacs/5.1.5_openmpi_gnu
+#gmx="gmx_mpi
+
+
+##--- ENIAC
+#gmx='/home/rafagp/exe/gromacs-5.1.4/build/bin/./gmx' #gmx_mpi
+
+
+
+file="escolhidos2.dat"
 tpr="../topol.tpr"
 xtc="../traj_comp.xtc"
 #reseta arquivo de saida
