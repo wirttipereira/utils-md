c0ef204e17101d14f67efec417038e5892df3736
wirttipereira <wirttipereira@wirttipereiras-MacBook-Pro.local>
2020-10-06 09:08:36 -0300

    script automatico

---
+++ plumed/n1114/4-bulk-grande-constraint/plot.p
@@ -0,0 +1,3 @@
+p for [i=2:4] 'COLVAR' u 1:i w l
+pause 10
+reread
---
+++ plumed/n1114/6-meta-dist/roda.sh
@@ -0,0 +1,3 @@
+gmx='/Users/wirttipereira/exe/gromacs-2019.4-plumed/build/bin/gmx'
+$gmx mdrun -nt 1 -v -s topol.tpr -nsteps 500000 -plumed plumed.dat 
+
