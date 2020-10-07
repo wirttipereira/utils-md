3d12ad65353c65693896d45cd8274c617ceb2dae
Rafael Guimaraes Pereira <wirttipereira@hotmail.com>
2020-10-07 12:55:37 -0300

    script automatico

--- umbrella-sampling/v2/v1/2-umbrella-md/1.5-roda-pbs.sh
+++
@@ -1,7 +0,0 @@
-#RGP - 25.8.2020
-#script para submeter jobs no ENIAC
-
-for i in {13000..23000..1000}
-do
-	qsub run$i.pbs
-done 
--- umbrella-sampling/v2/v2/2.5-cancela-jobs.sh
+++
@@ -1,6 +0,0 @@
-#RGP - 31.8.2020
-
-for i in {634981..635025..1}
-do
-	scancel $i
-done
