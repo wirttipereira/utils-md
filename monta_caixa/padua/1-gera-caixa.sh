cad3f90fef65a840220cd44d390d7136d8a95e6a
wirttipereira <wirttipereira@hotmail.com>
2020-10-19 08:19:25 -0300

    script automatico

--- monta_caixa/padua/1-gera-caixa.sh
+++ monta_caixa/padua/1-gera-caixa.sh
@@ -1,16 +1,21 @@
 #RGP - 6.Jan.2020
 # O parametro -b 10 eh para fazer uma caixa cubida de 45A de aresta.
 # Nao tem problema, eu vou editar o arquivo de saida no proximo passo, colocando o tamanho da caixa que me convier
+
+packmol='/home/rafagp/github/packmol/packmol'
+
 cp il-0.8.ff il.ff 
 
-~/Github/fftool/fftool  200 emim.zmat 200 ntf2.zmat -b 50
+./fftool 450 N1111.zmat 450 ntf2.zmat -b 50
+sleep 2
 
-sed -i -e 's/inside box.*$/inside box 0. 0. 0. 50.000 50.000  50.000/g' pack.inp
+sed -i -e 's/inside box.*$/inside box 0. 0. -70. 34.868  45.000  70.000/g' pack.inp
 
-~/Github/packmol/packmol < pack.inp
+$packmol < pack.inp
+sleep 2
 
-~/Github/fftool/fftool 200 emim.zmat 200 ntf2.zmat -b 50 -g
+./fftool 450 N1111.zmat 450 ntf2.zmat -b 34.868,45.000,220.000 -l
 
-sed -i -e 's/50.000   50.000   50.000/50.000   50.000   50.000/g' config.pdb
-
-sed -i -e 's/simbox/tma-emim:ntf2 RGP/g' field.top
+##Gromacs
+#sed -i -e 's/50.000   50.000   50.000/50.000   50.000   50.000/g' config.pdb
+#sed -i -e 's/simbox/N1111:Tf2N RGP/g' field.top
