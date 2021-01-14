e65c12b2c76bd9a51b47ed65d6ffabc91f8699e5
Rafael Guimaraes Pereira <wirttipereira@hotmail.com>
2021-01-14 13:49:24 -0300

    script automatico

---
+++ pylat-modificacoes/pylat.sh
@@ -0,0 +1,1 @@
+python /scratch/ilsupercap/rafael.pereira2/exe/PyLAT/PyLAT.py -c -g -d -m -T 400 -v 2 --mol ntf2 --mol tma --nummol 200 --nummol 200 ../log.lammps ../data.eq.lmp-npt ../dump.lammpstrj 
